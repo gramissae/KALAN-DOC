6f75eb84c717fb657a8635126f564b9bc84e69e0
Gino Missael Romero Andriano <gmr000p@MX.wal-mart.com>
2019-09-27 14:36:05 -0500

    Agregoo doc

--- api.js
+++ api.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const settings = require('./settings/secure');
+var _ = require('express-jwt');
 
+router.get('/login_token', async (req,res) => {
+    try{
+        let signature = new Object({
+            name: "myname",
+            id: "client"
+        });
 
-router.get('/')
-
+        const token = await jwt.sign(signature,settings.provider_key);
+        res.json({token: token})
+    }catch(error){
+        res.json(error);
+    }
+});
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 /* En este archivo realizare un servidor de muestra */
 
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const app = express();
+var engines = require('consolidate');
+var opn = require('opn');
 var ip = require('ip');
 
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 8080);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.engine('html', engines.mustache);
+app.set("view engine","html");
 app.use(morgan('dev'));
+
+app.use(express.static('views'));
 
 app.use('/api', require('./api'));
 
+//opn(`http://localhost:${app.get('port')}`);
+
 app.listen(app.get('port'), () => {
-    console.log('\x1b[32m%s\x1b[0m', `\nIngrese a LOCAL HTTP: http://${ip.address()}:${app.get('port')}\n`)
+    console.log('\x1b[32m%s\x1b[0m', `\nIngrese a LOCAL HTTP: http://${ip.address()}:${app.get('port')}\n`);
 });
 
